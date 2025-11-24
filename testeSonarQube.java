e337a4eceb62874a98cecbbdde82fbb380397415
Breno Amorim <breno.abarbosa@outlook.com>
2025-11-24 20:49:46 -0300

    teste SonarQube

--- testeSonarQube.java
+++ testeSonarQube.java
@@ -4,6 +4,15 @@ public class testeSonarQube {
         for (;i < 11; i++) {
            System.out.println(i); 
         }
+
     }
-    testetesteteste
+    public static int metodo(){
+        int a = 3;
+
+        for (int i = 0; i < 5; i--) {
+            a = i * a - 2;
+        }
+
+        return a;
+    }
 }
